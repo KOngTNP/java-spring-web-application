74a20045933d7547bdcb1c483579960bfc7a01ad
KOngTNP <tanapollll23@gmail.com>
2022-02-09 12:23:31 +0700

    add first-commit

---
+++ src/main/java/ku/kinkao/KinkaoApplication.java
@@ -0,0 +1,16 @@
+package ku.kinkao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinkaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinkaoApplication.class, args);
+	}
+
+}
+
+
+// command to run project on terminal : "mvn spring-boot:run"
